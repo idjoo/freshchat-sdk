71f9b7d0592ea601acca7115f98313d71e2fe2c7
Adrianus Vian Habirowo <adrianus.vian.habirowo@devoteam.com>
2025-02-09 12:43:25 +0700

    feat: add properties to the update conversation request object

--- freshchat/models/__init__.py
+++ freshchat/models/__init__.py
@@ -7,6 +7,7 @@ from .reply_conversation_response import ReplyConversationResponse
 from .reply_conversation_response_message_parts_item import ReplyConversationResponseMessagePartsItem
 from .reply_conversation_response_message_parts_item_text import ReplyConversationResponseMessagePartsItemText
 from .update_conversation_request import UpdateConversationRequest
+from .update_conversation_request_properties import UpdateConversationRequestProperties
 from .update_conversation_request_status import UpdateConversationRequestStatus
 from .update_conversation_response import UpdateConversationResponse
 
@@ -18,6 +19,7 @@ __all__ = (
     "ReplyConversationResponseMessagePartsItem",
     "ReplyConversationResponseMessagePartsItemText",
     "UpdateConversationRequest",
+    "UpdateConversationRequestProperties",
     "UpdateConversationRequestStatus",
     "UpdateConversationResponse",
 )
--- freshchat/models/update_conversation_request.py
+++ freshchat/models/update_conversation_request.py
@@ -1,10 +1,14 @@
-from typing import Any, TypeVar, Union
+from typing import TYPE_CHECKING, Any, TypeVar, Union
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
 
 from ..models.update_conversation_request_status import UpdateConversationRequestStatus
 from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+    from ..models.update_conversation_request_properties import UpdateConversationRequestProperties
+
 
 T = TypeVar("T", bound="UpdateConversationRequest")
 
@@ -15,15 +19,23 @@ class UpdateConversationRequest:
     Attributes:
         assigned_group_id (Union[Unset, str]): Identifier of the group to which the conversation is assigned, auto-
             generated when a group is created in the Freshchat system.
+        properties (Union[Unset, UpdateConversationRequestProperties]): Details of the conversation properties
+            configured for the Freshchat account. The attribute contains the names of the conversation properties configured
+            and the corresponding values populated, as a valid JSON object of key - value pairs.
         status (Union[Unset, UpdateConversationRequestStatus]): Status of the conversation.
     """
 
     assigned_group_id: Union[Unset, str] = UNSET
+    properties: Union[Unset, "UpdateConversationRequestProperties"] = UNSET
     status: Union[Unset, UpdateConversationRequestStatus] = UNSET
     additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> dict[str, Any]:
         assigned_group_id = self.assigned_group_id
+
+        properties: Union[Unset, dict[str, Any]] = UNSET
+        if not isinstance(self.properties, Unset):
+            properties = self.properties.to_dict()
 
         status: Union[Unset, str] = UNSET
         if not isinstance(self.status, Unset):
@@ -34,6 +46,8 @@ class UpdateConversationRequest:
         field_dict.update({})
         if assigned_group_id is not UNSET:
             field_dict["assigned_group_id"] = assigned_group_id
+        if properties is not UNSET:
+            field_dict["properties"] = properties
         if status is not UNSET:
             field_dict["status"] = status
 
@@ -41,8 +55,17 @@ class UpdateConversationRequest:
 
     @classmethod
     def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
+        from ..models.update_conversation_request_properties import UpdateConversationRequestProperties
+
         d = src_dict.copy()
         assigned_group_id = d.pop("assigned_group_id", UNSET)
+
+        _properties = d.pop("properties", UNSET)
+        properties: Union[Unset, UpdateConversationRequestProperties]
+        if isinstance(_properties, Unset):
+            properties = UNSET
+        else:
+            properties = UpdateConversationRequestProperties.from_dict(_properties)
 
         _status = d.pop("status", UNSET)
         status: Union[Unset, UpdateConversationRequestStatus]
@@ -53,6 +76,7 @@ class UpdateConversationRequest:
 
         update_conversation_request = cls(
             assigned_group_id=assigned_group_id,
+            properties=properties,
             status=status,
         )
 
